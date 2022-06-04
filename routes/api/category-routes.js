cb0e619d39eff39f09286245791c720957cb6c54
rrangel07 <99489197+rrangel07@users.noreply.github.com>
2022-06-04 12:57:11 -0400

    Wrking on get and get id routes for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,13 +15,32 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id,{
+      // be sure to include its associated Products
+      include: [{ model: Product }],
+    });
+    if(!categoryData){
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {

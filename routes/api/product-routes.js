716eaced7f08a20b34e6fbb16f0e2f32e2ccf33d
rrangel07 <99489197+rrangel07@users.noreply.github.com>
2022-06-04 16:47:00 -0400

    Product post route improved

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -37,40 +37,59 @@ router.get('/:id', async (req, res) => {
 });
 
 // create new product
-router.post('/', (req, res) => {
+// create new product
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
       price: 200.00,
       stock: 3,
-      tagIds: [1, 2, 3, 4]
+      category_id: 
+      tag_id: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
+    try {
+      const productData = await Product.create(req.body);
+      if (req.body.tag_id.length) {
+        const productTagIdArr = req.body.tag_id.map((tag_id) => {
           return {
-            product_id: product.id,
+            product_id: productData.id,
             tag_id,
           };
         });
-        return ProductTag.bulkCreate(productTagIdArr);
+        await ProductTag.bulkCreate(productTagIdArr);
       }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+      res.status(200).json(productData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update product data
+  try {
+    const categoryData = await Product.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: 
+      {
+        id: req.params.id,
+      },
+    });
+    console.log(categoryData);
+    if(!categoryData[0]){
+      res.status(400).json({ message: 'Nothing to update' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+
   Product.update(req.body, {
     where: {
       id: req.params.id,

7e8ff139656ad440ded4e27ade39d05aecf19896
rrangel07 <99489197+rrangel07@users.noreply.github.com>
2022-06-04 20:20:49 -0400

    Product delete route implemented

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -119,8 +119,25 @@ router.put('/:id', async (req, res) => {
 
 
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy(
+    {
+      where: 
+      {
+        id: req.params.id,
+      },
+    });
+    console.log(productData);
+    if(!productData){
+      res.status(400).json({ message: 'Nothing to delete' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
